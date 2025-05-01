2bd613a533ed91f5b0a0291eb393d733dfe99c4c
belalE <belalelsiesy@gmail.com>
2025-05-01 10:24:18 -0500

    Add shape spawner controller with cube, sphere, and cylinder functionality

---
+++ Assets/CreateShapePrefabs.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class CreateShapePrefabs : MonoBehaviour
+{
+    [ContextMenu("Create Shape Prefabs")]
+    public void CreatePrefabs()
+    {
+        // Create Sphere
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        SetupShapePrefab(sphere, "DraggableSphere");
+        DestroyImmediate(sphere);
+
+        // Create Cylinder
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        SetupShapePrefab(cylinder, "DraggableCylinder");
+        DestroyImmediate(cylinder);
+    }
+
+    private void SetupShapePrefab(GameObject shape, string prefabName)
+    {
+        // Add required components
+        shape.AddComponent<CubeMover>();
+        shape.AddComponent<RotateSelectedObject>();
+        shape.AddComponent<ResizeOnScroll>();
+
+        // Set up collider
+        Collider collider = shape.GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.isTrigger = false;
+        }
+
+        // Set up renderer
+        Renderer renderer = shape.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = new Material(Shader.Find("Standard"));
+            renderer.material.color = new Color32(182, 189, 226, 255);
+        }
+
+        // Set tag
+        shape.tag = "Selectable";
+
+        // Save as prefab
+        #if UNITY_EDITOR
+        UnityEditor.PrefabUtility.SaveAsPrefabAsset(shape, $"Assets/{prefabName}.prefab");
+        #endif
+    }
+} 
---
+++ Assets/ShapeSpawnerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ShapeSpawnerController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+    public GameObject cylinderPrefab;
+    public Transform spawnPoint;
+
+    // Default color based on your screenshot (B6BDE2)
+    private Color defaultColor = new Color32(182, 189, 226, 255);
+
+    public void SpawnCube()
+    {
+        SpawnShape(cubePrefab);
+    }
+
+    public void SpawnSphere()
+    {
+        SpawnShape(spherePrefab);
+    }
+
+    public void SpawnCylinder()
+    {
+        SpawnShape(cylinderPrefab);
+    }
+
+    private void SpawnShape(GameObject prefab)
+    {
+        if (prefab != null)
+        {
+            Vector3 spawnPosition = spawnPoint != null ? spawnPoint.position : Vector3.zero;
+
+            GameObject newShape = Instantiate(prefab, spawnPosition, Quaternion.identity);
+            newShape.tag = "Selectable";
+
+            Renderer rend = newShape.GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material.color = defaultColor;
+            }
+        }
+    }
+}
